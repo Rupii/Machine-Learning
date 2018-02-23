cab79362ffea605c34ba230c5e4de1f09d623b8b
rupii <mudavathrupesh@gmail.com>
2018-02-23 22:25:34 +0530

    'preprocessing'

--- data_preprocessing/data.preprocessing.R
+++ data_preprocessing/data.preprocessing.R
@@ -1,8 +1,10 @@
 # Data preprocessing
 
 #importing data
+
 df <- read.csv("Data.csv")
 head(df)
+
 
 #imputing the missing values
 df$Age = ifelse(is.na(df$Age), 
@@ -13,4 +15,24 @@ df$Salary = ifelse(is.na(df$Salary),
                 ave(df$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                 df$Salary)
 
-print(df)
+# Categorical variables
+
+df$Country <- factor(df$Country,
+                     levels = c('France', 'Spain', 'GErmany'),
+                     labels = c(1, 2, 3))
+
+df$Purchased <- factor(df$Purchased)
+
+
+# spliting into train test
+install.packages('caTools')
+require(caTools)
+set.seed(101)
+# unlike in python we need to do some manual work in R
+split = sample.split(df$Purchased, SplitRatio = 0.8)
+train_set = subset(df, split == TRUE) 
+test_set = subset(df, split == FALSE)
+
+# feature scaling
+train_set[,2:3] = scale(train_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -23,3 +23,31 @@ impute = impute.fit(X[:, 1:3])
 X[:, 1:3] = impute.transform(X[:, 1:3])
 
 print(X)
+
+# categorical variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+X_cat = LabelEncoder()
+X[:, 0] = X_cat.fit_transform(X[:, 0])
+# labels in ordered way
+# i.e., france 3 germany 0
+# hot encoding / dummy variables 
+
+onehot = OneHotEncoder(categorical_features = [0])
+X = onehot.fit_transform(X).toarray()
+
+y_cat = LabelEncoder()
+y = y_cat.fit_transform(y)
+
+# train testt split
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+
+
+# preprocessing 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
